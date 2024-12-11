31bbbc766b0310b0512491114150f513678926aa
Sachin Sajwan <samsajwan1996@gmail.com>
2024-12-11 22:23:00 +0530

    python unittest 1

---
+++ rearrange2.py
@@ -0,0 +1,2 @@
+from rearrange import rearrange_name
+print(rearrange_name("Lovelace, Ada"))
---
+++ rearrange_unittest.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+import unittest
+
+from rearrange import rearrange_name
+
+class TestRearrange(unittest.TestCase):
+    
+  def test_basic(self):
+    testcase = "Lovelace, Ada"
+    expected = "Ada Lovelace"
+    self.assertEqual(rearrange_name(testcase), expected)
+
+  def test_empty(self):
+    testcase = ""
+    expected = ""
+    self.assertEqual(rearrange_name(testcase), expected)
+
+  def test_double_name(self):
+    testcase = "Hopper, Grace M."
+    expected = "Grace M. Hopper"
+    self.assertEqual(rearrange_name(testcase), expected)
+
+  def test_one_name(self):
+    testcase = "Voltaire"
+    expected = "Voltaire"
+    self.assertEqual(rearrange_name(testcase), expected)
+
+
+# Run the tests
+unittest.main()
